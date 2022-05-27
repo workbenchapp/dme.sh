ce0b0d1678de2bacfdbab3c23cf1323a118e8f81
Sven Dowideit <SvenDowideit@home.org.au>
2022-05-27 17:30:44 +1000

    lets see if just deleting the sharp plugins gets it to build on netlify
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,14 +3,14 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `dme.sh`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://dme.sh`
   },
   plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, "gatsby-plugin-mdx", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
