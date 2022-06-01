aca18eea034c206fe36a15c77eb69af40d4ade80
Sven Dowideit <SvenDowideit@home.org.au>
2022-06-01 13:45:39 +1000

    add wallet-adapter
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,19 @@ import * as React from "react";
 import { Container } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
 function Header() {
   return (
     <Navbar fixed="bottom" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">DMe.sh</Navbar.Brand>
+        <WalletMultiButton />
+        <WalletDisconnectButton />
       </Container>
     </Navbar>
   );
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { FC, useMemo } from "react";
 import Header from "../components/Header";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+// Default styles that can be overridden by your app
+import("@solana/wallet-adapter-react-ui/styles.css");
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/index.css";
 
 // eslint-disable-next-line react/prop-types
-const Layout = ({ children }) => (
-  <div>
-    <Header />
-    <div className="container">
-      <div className="PageContent">{children}</div>
-    </div>
-  </div>
-);
+const Layout = ({ children }) => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Header />
+          <div className="container">
+            <div className="PageContent">{children}</div>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
 
 export default Layout;
