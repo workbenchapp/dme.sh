3e7b51c065f69486dc66a91646a745a037035f1c
Blusk <bluskript@gmail.com>
2022-08-23 09:22:25 -0400

    add dprofile

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,21 @@
 const webpack = require("webpack");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-        // https://joseph.to/web3js-on-gatsby/
-        plugins: [
-            new webpack.ProvidePlugin({
-                Buffer: [require.resolve("buffer"), "Buffer"],
-            }),
-        ],
-        resolve: {
-            fallback: {
-                crypto: require.resolve("crypto-browserify"),
-                stream: require.resolve("stream-browserify"),
-                //buffer: require.resolve('buffer/'),
-            },
-        },
-    })
-}
+  actions.setWebpackConfig({
+    // https://joseph.to/web3js-on-gatsby/
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: [require.resolve("buffer"), "Buffer"],
+      }),
+    ],
+    resolve: {
+      fallback: {
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        zlib: require.resolve("browserify-zlib"),
+        path: require.resolve("path-browserify"),
+        //buffer: require.resolve('buffer/'),
+      },
+    },
+  });
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
+import { EditableDProfile } from "dprofile";
+import "dprofile/dist/style.css";
 import * as React from "react";
 import { Container } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { PublicKey } from "@solana/web3.js";
+
+const REALM_CREATOR = new PublicKey(
+  "xMUbkZmXfNtnWDMtXuVC9dc3A3vDMMSrsLLnz5kjoSQ"
+);
 
 function Header() {
   return (
@@ -14,6 +17,7 @@ function Header() {
       <Container>
         <Navbar.Brand href="/">DMe.sh</Navbar.Brand>
         <WalletMultiButton />
+        <EditableDProfile size={48} creator={REALM_CREATOR} />
       </Container>
     </Navbar>
   );
